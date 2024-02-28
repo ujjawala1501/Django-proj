33f40a07ec396f063487286639fae410f82ff996
Ujjawala Katakwar <ujjawala1501@gmail.com>
2024-02-28 23:47:38 +0530

    added urls in Home

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls), #Whenever there is a /admin in front of server port send them to django admin
-    path('',)
+    path('',include('Home.urls'))
 ]
--- Hello/Home/urls.py
+++ Hello/Home/urls.py
@@ -1,7 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path
+from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("",views.index,name='home')
 ]
