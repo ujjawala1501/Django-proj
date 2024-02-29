d03d6b0f4228754ad7dcac790babcee64674adb4
Ujjawala Katakwar <ujjawala1501@gmail.com>
2024-02-29 23:25:45 +0530

    added more links for pages

--- Hello/Home/urls.py
+++ Hello/Home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from Home import views
 
 urlpatterns = [
-    path("",views.index,name='home')
+    path("",views.index,name='home'),
+    path("",views.about,name='about'),
+    path("",views.services,name='services'),
+    path("",views.contact,name='contact')
 ]
